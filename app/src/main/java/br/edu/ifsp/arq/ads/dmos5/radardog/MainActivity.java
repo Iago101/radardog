c44628b9fbb00726d2d7687e8da4536aafc82b36
iago moraes <iagolol@outlook.com>
2022-11-25 17:39:05 -0300

    splash art added, theme locked on colors.xml

--- app/src/main/java/br/edu/ifsp/arq/ads/dmos5/radardog/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/arq/ads/dmos5/radardog/MainActivity.java
@@ -1,10 +1,9 @@
 package br.edu.ifsp.arq.ads.dmos5.radardog;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
