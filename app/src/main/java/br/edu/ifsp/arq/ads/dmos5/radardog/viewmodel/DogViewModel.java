c4ae62551426d40c41d938507a7589a9e857b0a1
iago moraes <iagolol@outlook.com>
2022-12-09 19:56:36 -0300

    dog db part
    dogviewmodel is broken

---
+++ app/src/main/java/br/edu/ifsp/arq/ads/dmos5/radardog/viewmodel/DogViewModel.java
@@ -0,0 +1,43 @@
+package br.edu.ifsp.arq.ads.dmos5.radardog.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.edu.ifsp.arq.ads.dmos5.radardog.model.Dog;
+import br.edu.ifsp.arq.ads.dmos5.radardog.repository.DogsRepository;
+
+public class DogViewModel extends AndroidViewModel {
+
+    private DogsRepository dogsRepository;
+
+    public DogViewModel(@NonNull Application application) {
+        super(application);
+        dogsRepository = new DogsRepository(application);
+    }
+
+    public void createActivity(Dog dog){
+        dogsRepository.insert(activity);
+    }
+
+    public void updateActivity(Dog dog){
+        dogsRepository.update(activity);
+    }
+
+    public void deleteActivity(Dog dog){
+        dogsRepository.delete(activity);
+    }
+
+    public LiveData<List<Dog>> allDogs(String dogId){
+        return dogsRepository.getAllDogs(dogID);
+    }
+
+
+}
